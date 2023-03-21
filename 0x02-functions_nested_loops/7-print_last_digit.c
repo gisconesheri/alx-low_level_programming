f306ef06f766d152a61e65642f9d7fc4af34179e
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-03-21 20:42:09 +0000

    fixing 7;s eerrors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_last_number - prints the last number in the provided integer
+ * print_last_digit - prints the last number in the provided integer
  * @n: number to work on
  *
  * Return: The last digit of the number provided without a sign
  */
 
-int print_last_number(int n)
+int print_last_digit(int n)
 {
 	/* call the absolute function defined in 6-abs.c*/
 	int absN;
