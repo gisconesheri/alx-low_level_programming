e565bdd9f6ee6a48b911856479458467944528a2
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-03-22 15:50:12 +0000

    fix leap year code

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -49,7 +49,7 @@ void normal_year_calcs(int month, int day, int year)
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 400 == 0 || year % 100 == 0)
+	if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 != 0)))
 	{
 		leap_year_calcs(month, day);
 	}
