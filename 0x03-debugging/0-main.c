e94833d411fa48054e917b98e2cd01b192ae5289
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-03-22 13:13:16 +0000

    multiple mains assignement

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -13,5 +14,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0):
+	return (0);
 }
