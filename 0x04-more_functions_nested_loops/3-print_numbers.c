cf0937da7a4ac7fd4dd69241484e236621df90b2
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-03-23 13:21:17 +0000

    wrote code for function print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
