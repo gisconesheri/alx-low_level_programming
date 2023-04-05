cfa4a04ef9fc78040d3d3e4eaf66d7fc1e924719
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-05 20:19:44 +0000

    fixed fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,25 @@ int main(void)
 	int num;
 
 	num = 1;
+	printf("%d", n);
 
 	while (num < 101)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf(" %d", num);
 		}
 		num++;
 	}
