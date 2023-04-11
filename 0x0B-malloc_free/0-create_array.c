e1345d11ef316011f10bee4a021dbd67129c67cb
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-11 20:21:44 +0000

    malloc excercises

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,12 @@ char *create_array(unsigned int size, char c)
 	char *str_array;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	str_array = malloc(size * sizeof(char));
+	if (str_array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
