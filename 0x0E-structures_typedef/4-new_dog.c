5724be177533df8754abe401e3b4a43975446073
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-17 22:13:34 +0000

    exercises for preprocessor and structs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,10 +12,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
 
-	dog1 = malloc(sizeof(dog1) + sizeof(age) + sizeof(dog1));
+	dog1 = malloc(sizeof(*dog1));
 	if (dog1 == NULL)
 		return (NULL);
-	
 	dog1->name = name;
 	dog1->age = age;
 	dog1->owner = owner;
