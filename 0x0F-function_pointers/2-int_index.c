4d4c575c07b8530e00933356efcff504be7ac22e
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-04-19 20:23:50 +0000

    fix code for excercise 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * int_index - searches for an integer
  * @array: array of integers to search through
@@ -12,6 +13,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
