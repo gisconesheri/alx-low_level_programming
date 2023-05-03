960e40d50c9e76ffeafbab7bb186c19992567689
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-05-03 13:08:58 +0000

    exercises on singly linked list containing integers

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees the memory allocated in creating a singly linked list
+ * @head: pointer to the first node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node = head;
+	int size = 0;
+	int i;
+
+	while (current_node->next != NULL)
+	{
+		size++;
+		current_node = current_node->next;
+	}
+	for (; size >= 0; size--)
+	{
+		current_node = head;
+		i = 0;
+		while (i < size)
+		{
+			current_node = current_node->next;
+			i++;
+		}
+		free(current_node);
+	}
+
+}
