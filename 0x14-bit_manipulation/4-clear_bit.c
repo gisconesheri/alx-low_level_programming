ff39b8c1182a2cd146813ebce5a666fd45279d32
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-05-05 02:47:36 +0000

    wrote functinality for clearing a bit to 0 at a given location

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -96,27 +96,3 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	free(true_binary);
 	return (1);
 }
-
-#include <stdio.h>
-#include "main.h"
-
-/**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	    unsigned long int n;
-
-	        n = 1024;
-		    clear_bit(&n, 10);
-		        printf("%lu\n", n);
-			    n = 0;
-			        clear_bit(&n, 10);
-				    printf("%lu\n", n);
-				        n = 98;
-					    clear_bit(&n, 1);
-					        printf("%lu\n", n);
-						    return (0);
-}
