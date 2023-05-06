cb76cdfb82c95e69ad82578e3e335fa1d09f0e5c
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-05-06 17:17:58 +0000

    change various files to meet checker requirements

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,54 +1,83 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
-
-char *get_binary(unsigned long int num)
+#include <stdlib.h>
+/**
+ * exponent_two - returns 2 to the power of times
+ * @times: the exponent
+ * Return: result of 2 to the power of 'times'
+ */
+unsigned long int exponent_two(unsigned long int times)
 {
-	char *binary_string;
-	unsigned long int i , rem = 0;
-
-	binary_string = malloc(sizeof(char) * 65);
-
-	while (i <= 64)
-	{
-		rem = num % 2;
-		if (rem == 0)
-			binary_string[i] = 48;
-		else if (rem == 1)
-			binary_string[i] = 49;
-		else
-			binary_string[i] = 48;
-		num = num >> 1;
-		i++;
-	}
-	printf("n in binary is %c\n", binary_string[34]);
-
-	return (binary_string);
+	if (times == 0)
+		return (1);
+	return (2 * exponent_two(times - 1));
 }
+/**
+ * get_binary - convert a number into binary
+ * @n: number to convert
+ * Return: pointer to an array of 1s and 0s representing n
+ */
+int *get_binary(unsigned long int n)
+{
+	unsigned long int temp_n2 = n;
+	unsigned long int temp_n;
+	unsigned long int times, i = 0;
+	int *binary_number;
 
+	binary_number = malloc(sizeof(int) * 64);
+	if (binary_number == NULL)
+		return (NULL);
+
+	for (; i < 64; i++)
+		binary_number[i] = 0;
+
+	if (n == 1)
+		binary_number[0] = 1;
+	while (temp_n2 > 1)
+	{
+		temp_n = temp_n2;
+		times = 0;
+		while (temp_n > 1)
+		{
+			temp_n = temp_n >> 1;
+			times++;
+		}
+
+		binary_number[times] = 1;
+		temp_n2 = temp_n2 - (exponent_two(times));
+	}
+
+	if (temp_n2 == 1)
+		binary_number[0] = 1;
+	return (binary_number);
+}
+/**
+ * flip_bits - count the number of bits needed to be flipped
+ * to get two numbers to be the same in binary
+ * @n: number 1
+ * @m: number 2
+ * Return: number of bits to be flipped
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	char *binary_one;
-	char *binary_two;
-	unsigned long int n1 = n;
-	unsigned long int m1 = m;
+	int *n_binary;
+	int *m_binary;
+	int i = 0;
+	unsigned int bits_to_flip = 0;
 
-	binary_one = malloc(sizeof(char) * 65);
-	binary_two = malloc(sizeof(char) * 65);
-	
-	binary_one = get_binary(n1);
-	binary_two = get_binary(m1);
+	n_binary = get_binary(n);
+	m_binary = get_binary(m);
 
-	printf("n in binary is %c\n", binary_one[1]);
-	printf("m in binary is %c\n", binary_two[1]);
-
-	if (binary_one == NULL || binary_two == NULL)
+	if (n_binary == NULL || m_binary == NULL)
 		return (0);
 
-	return (0);
-}
-
-int main(void)
-{
-	flip_bits(1024, 1);
-	return (0);
+	while (i < 64)
+	{
+		if (n_binary[i] != m_binary[i])
+		{
+			bits_to_flip++;
+		}
+		i++;
+	}
+	return (bits_to_flip);
 }
